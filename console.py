4ba62c4aa86a552391498c62bd301013c46dab26
smartraysam <raysamtob@gmail.com>
2023-08-13 21:13:52 +0100

    task 6

--- console.py
+++ console.py
@@ -1,3 +1,32 @@
 #!/usr/bin/python3
 """The Command Line Interpreter."""
 
+from models import *
+import json
+import cmd
+import re
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class that defines Airbnb clone public class instances."""
+
+    prompt = "(hbnb) "
+
+    def do_EOF(self, line):
+        """Function handle end of file character (Ctrl+D)."""
+
+        print()
+        return (True)
+
+    def do_quit(self, line):
+        """Function that exits the shell in interactive mode."""
+
+        return (True)
+
+    def emptyline(self):
+        """Function that does nothing on ENTER."""
+
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
